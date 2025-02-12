0a497259288644983f0c36868e81522db472c59d
Artem Gryniv <artem.gryniv@icloud.com>
2025-02-12 14:05:16 -0600

    updated tsp

--- TSP Converting.py
+++ TSP Converting.py
@@ -1,11 +1,11 @@
 import xml.etree.ElementTree as ET
 
 # Load and parse the XML file
-tree = ET.parse('/Users/artemgryniv/Library/Mobile Documents/com~apple~CloudDocs/Graph Partitioning/kroC100.xml')
+tree = ET.parse('path/to/your/.xml/file')
 root = tree.getroot()
 
 # Open a file to write the output
-with open('/Users/artemgryniv/Library/Mobile Documents/com~apple~CloudDocs/Graph Partitioning/downloaded_graphs/kroC100.txt', 'w') as output_file:
+with open('path/to/your/.txt/file', 'w') as output_file:
 
     for i, vertex in enumerate(root.findall('.//vertex')):
         vertex_index = i + 1
